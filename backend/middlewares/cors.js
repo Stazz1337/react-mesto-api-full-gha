1992c5b63c45b26913de5e728ccb9e9ce013ee48
Stas Tolmachev <stusik@yandex.ru>
2023-07-11 01:17:51 +0300

    feat: add jwt2

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,6 +1,7 @@
 // Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
   'http://localhost:3000',
+  'http://localhost:3001',
   'http://stazzler.nomoredomains.work',
   'https://stazzler.nomoredomains.work',
 ];
