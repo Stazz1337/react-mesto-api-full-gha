e2d296e0bc878cec26cea4938fca09cba189e508
Stas Tolmachev <stusik@yandex.ru>
2023-07-11 13:59:28 +0300

    feat: add jwt5

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -10,6 +10,12 @@ const {
 
 const NotFoundError = require('../errors/NotFoundError');
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 router.post('/signin', validateUserBody, login);
 
 router.post('/signup', validateAuth, createUser);
